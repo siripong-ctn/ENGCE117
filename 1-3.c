65ca7e0b1189d0e272c2757823c388d0b85eee6a
siripong-ctn <siripong.2320@gmail.com>
2025-11-20 00:11:08 +0700

    Update new 1-3

---
+++ 1-3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, operationCode, result;
+    
+    // รับค่าตัวเลข 2 ตัวและรหัสคำสั่ง
+    scanf("%d %d %d", &num1, &num2, &operationCode);
+    if (operationCode == 1) {
+        result = num1 + num2;
+        printf("%d", result);
+    } else if (operationCode == 2) {
+        result = num1 - num2;
+        printf("%d", result);
+    } else if (operationCode == 3) {
+        result = num1 * num2;
+        printf("%d", result);
+    } else if (operationCode == 4) {
+        result = num1 / num2;
+        printf("%d", result);
+    } else {
+        printf("Invalid Operation");
+    }
+
+    // TODO: Implement the switch statement logic here, including break and default
+
+    return 0;
+}
