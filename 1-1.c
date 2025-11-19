1bafbad3e67c82e1cdee73ee2120e67b271f70a8
siripong-ctn <siripong.2320@gmail.com>
2025-11-19 23:08:40 +0700

    Update new 1-1

--- 1-1.c
+++ 1-1.c
@@ -1,16 +1,13 @@
-// Pre-code (โค้ดบังคับ)
 #include <stdio.h>
 int main() {
     int N, i;
     
     // รับค่า N (จำนวนรอบ)
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1 || N <= 0) {
         return 1; // Handle input failure
-    } else if (N <= 0) {
-        printf("Error");
     }
     
-    for (i = 1; i <= N; i++) {
+    for (i = 0; i <= N; i++) {
         printf("Hello Loop!\n");
     }
     // TODO: Implement the for loop to print the message N times
