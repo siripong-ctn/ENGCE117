6ea5176788660165784540840c1a300049f01a01
siripong-ctn <siripong.2320@gmail.com>
2025-11-20 12:38:07 +0700

    Update new 1-3

--- 1-3.c
+++ 1-3.c
@@ -5,20 +5,27 @@ int main() {
     
     // รับค่าตัวเลข 2 ตัวและรหัสคำสั่ง
     scanf("%d %d %d", &num1, &num2, &operationCode);
-    if (operationCode == 1) {
+
+    switch (operationCode) {
+    case 1:
         result = num1 + num2;
         printf("%d", result);
-    } else if (operationCode == 2) {
+        break;
+    case 2:
         result = num1 - num2;
         printf("%d", result);
-    } else if (operationCode == 3) {
+        break;
+    case 3:
         result = num1 * num2;
         printf("%d", result);
-    } else if (operationCode == 4) {
+        break;
+    case 4:
         result = num1 / num2;
         printf("%d", result);
-    } else {
+        break;
+    default :
         printf("Invalid Operation");
+        break;
     }
 
     // TODO: Implement the switch statement logic here, including break and default
