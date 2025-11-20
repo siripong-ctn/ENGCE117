495d8f293943c8563ed683f73355e595277f6e28
siripong-ctn <siripong.2320@gmail.com>
2025-11-20 17:39:43 +0700

    Update new 1-8

--- 1-8.c
+++ 1-8.c
@@ -4,29 +4,31 @@ int main() {
     int dayCode, hour;
     
     // รับค่ารหัสวันและชั่วโมง
-    scanf("%d %d", &dayCode, &hour);
-    switch (dayCode) {
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-        if (hour >= 8 && hour <= 17) {
-            printf("System Running (Workday)");
-        } else {
-            printf("System Idle (Off-hours)");
-        }
-        break;
-    case 6:
-    case 7:
-        printf("Weekend Relax Mode");
-        break;
-    default:
-        printf("Invalid Day Code");
-        break;
+    if (scanf("%d %d", &dayCode, &hour) != 2) {
+        return 1; // Handle input failure
     }
     
     // TODO: Implement the main switch statement with nested if-else logic inside cases 1-5
+    switch (dayCode) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            if (hour >= 8 && hour <= 17) {
+                printf("System Running (Workday)");
+            } else {
+                printf("System Idle (Off-hours)");
+            }
+            break;
+        case 6:
+        case 7:
+            printf("Weekend Relax Mode");
+            break;
+        default:
+            printf("Invalid Day Code");
+            break;
+    }
 
     return 0;
 }
