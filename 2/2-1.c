3c405707fbc67246d7755c27f1f0146e91611b51
siripong-ctn <siripong.2320@gmail.com>
2025-11-20 21:16:15 +0700

    Update new 2-1

--- 2/2-1.c
+++ 2/2-1.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
-
 int main() {
     int N, i;
     
     // รับค่า N (จำนวนรอบ)
-    if (scanf("%d", &N) != 1) {
+    if (scanf("%d", &N) != 1 || N <= 0) {
+        printf("Error");
         return 1; // Handle input failure
+    }
+    
+    for (i = 0; i < N; i++) {
+        printf("Hello Loop!\n");
     }
     
     // TODO: Implement the for loop to print the message N times
