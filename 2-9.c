6b33ba6c0e1875496a64b236125944c33c240147
siripong-ctn <siripong.2320@gmail.com>
2025-11-21 17:25:19 +0700

    Update new 2-9

--- 2-9.c
+++ 2-9.c
@@ -35,22 +35,28 @@ void CMDCode_Command() {
             currentStock += quantity;
             printf("Received %d units.\n", quantity);
             break;
+
         case 2:
             if (quantity <= 0) {
                 printf("Error: Quantity must be positive.");
-            } else if (quantity <= currentStock) {
+            } 
+            else if (quantity <= currentStock) {
                 currentStock -= quantity;
                 printf("Shipped %d units.", quantity);
-            } else if (quantity > currentStock) {
+            } 
+            else if (quantity > currentStock) {
                 totalPenalties += PENALTY_FEE;
                 printf("FAILURE: Insufficient stock. PENALTY %.2f added.", PENALTY_FEE);
             }
+
             printf("\n");
             break;
+
         case 3:
             printf("Current Stock: %d\n", currentStock);
             printf("Total Penalties: %.2f\n", totalPenalties);
             break;
+
         default:
             printf("Error: Invalid Command.\n");
             break;
