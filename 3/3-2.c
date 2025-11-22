50fbcc40503c012d1dce460767b0dcd7b176f9b9
siripong-ctn <siripong.2320@gmail.com>
2025-11-22 15:03:15 +0700

    Update 3-2

--- 3/3-2.c
+++ 3/3-2.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
-
 struct Student {
     char name[50];
     int studentId;
     float score;
 };
-
 int main() {
     int N, i;
-    
-    if (scanf("%d", &N) != 1) {
-        return 1;
-    }
-    
+    scanf("%d", &N);
     struct Student students[N];
-    
-    for (i = 0; i < N; i++) {
-        
-        if (scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3) {
-            return 1;
-        }
+    for (i=0;i<N;i++) {
+        scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name);
     }
-    
     for (i = 0; i < N; i++) {
-        
+        printf("ID: %d ,", students[i].studentId);
+        printf(" Score: %.2f ,", students[i].score);
+        printf(" Name: %s \n", students[i].name);
     }
-
     return 0;
 }
