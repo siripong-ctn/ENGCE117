204a047c688a742c69e18fd4b9b1f235f4e6f25f
siripong-ctn <siripong.2320@gmail.com>
2025-11-22 16:04:55 +0700

    Update 3-5

---
+++ 3/3-5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+struct Order {
+    int itemId;
+    float unitPrice;
+    int quantity;
+};
+int main() {
+    int N, i;
+    float grandTotal=0.0, SHIPPING_FEE = 50.0;
+
+    scanf("%d", &N);
+    struct Order orders[N];
+    
+    for (i = 0; i < N; i++) {
+        float itemTotal;
+        if (scanf("%d %f %d", &orders[i].itemId, &orders[i].unitPrice, &orders[i].quantity) != 3) {
+            return 1;
+        }
+        itemTotal = (orders[i].unitPrice * orders[i].quantity) + SHIPPING_FEE;
+        if (itemTotal >= 500.0) {
+            itemTotal *= 0.9;
+        } 
+        grandTotal += itemTotal;
+    }
+    
+    printf("Grand Total: %.2f\n", grandTotal);
+    return 0;
+}
