4c6a9a13890b116e4c4e824862c72b56d40872dd
siripong-ctn <siripong.2320@gmail.com>
2025-11-24 19:50:22 +0700

    Update 4-9

--- 4/4-9.c
+++ 4/4-9.c
@@ -3,14 +3,14 @@ int gross_salary;
 float tax_rate = 0.0;
 float tax_amount = 0.0;
 float net_salary = 0.0;
+void calculate_tax();
 
 int main() {
     
     printf("(THB/month, integer): ");
     scanf("%d", &gross_salary);
 
-    tax_amount = gross_salary * tax_rate;
-    net_salary = (float)gross_salary - tax_amount;
+    calculate_tax();
     
     printf("---SALARY CALCULATION REPORT---\n");
     printf("Gross Salary: %d THB\n", gross_salary);
@@ -19,4 +19,18 @@ int main() {
     printf("Net Salary: %.2f THB\n", net_salary);
 
     return 0;
+}
+
+void calculate_tax() {
+    if (gross_salary < 20000) {
+        tax_rate = 0.0;
+    } else if (gross_salary >= 20000 && gross_salary < 50000) {
+        tax_rate = 0.05;
+    } else if (gross_salary >= 50000) {
+        tax_rate = 0.10;
+    } else {
+        tax_rate = 0.15;
+    }
+    tax_amount = gross_salary * tax_rate;
+    net_salary = (float)gross_salary - tax_amount;
 }
