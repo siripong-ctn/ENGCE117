eccae6bfb7920ed4cc4777da3a62defce8bf83e3
siripong-ctn <siripong.2320@gmail.com>
2025-11-26 17:06:54 +0700

    Update 5-9

--- 5/5-9.c
+++ 5/5-9.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-struct Product
-{
+struct Product {
     int item_id ,stock_quantity;
     float cost_price, sell_price;
 };
@@ -9,38 +8,22 @@ struct Product
 float calculate_total_profit(float cost, float sell, int qty);
 float calculate_profit_percentage(float cost, float sell);
 
-int main() {
-    struct Product item;
-    float total_profit, profit_percent;
+struct Product item;
+float total_profit, profit_percent;
+void check_output();
+void input_data();
+void printf_type();
 
-    printf("Enter ID: ");
-    scanf("%d", &item.item_id);
-    printf("Enter Cost Price: ");
-    scanf("%f", &item.cost_price);
-    printf("Enter Sell Price: ");
-    scanf("%f", &item.sell_price);
-    printf("Enter Stock Quantity: ");
-    scanf("%d", &item.stock_quantity);
+int main() {
+
+    input_data();
 
     total_profit = calculate_total_profit(item.cost_price, item.sell_price, item.stock_quantity);
     profit_percent = calculate_profit_percentage(item.cost_price, item.sell_price);
 
-    printf("INVENTORY PROFIT REPORT\n");
-    printf("ID: %d\n", item.item_id);
-    printf("Cost: %.2f\n", item.cost_price);
-    printf("Sell: %.2f\n", item.sell_price);
-    printf("Quantity: %d\n", item.stock_quantity);
-    printf("Total Profit: %.2f\n", total_profit);
-    printf("Profit Percentage: %.2f%%\n", profit_percent);
+    printf_type();
 
-    printf("Status: ");
-    if (profit_percent > 0) {
-        printf("Profit");
-    } else if (profit_percent < 0) {
-        printf("Loss");
-    } else {
-        printf("Break Even");
-    }
+    check_output();
 
     return 0;
 }
@@ -51,4 +34,36 @@ float calculate_total_profit(float cost, float sell, int qty) {
 
 float calculate_profit_percentage(float cost, float sell) {
     return ((sell - cost) / cost) * 100;
+}
+
+void input_data() {
+    printf("Enter ID: ");
+    scanf("%d", &item.item_id);
+    printf("Enter Cost Price: ");
+    scanf("%f", &item.cost_price);
+    printf("Enter Sell Price: ");
+    scanf("%f", &item.sell_price);
+    printf("Enter Stock Quantity: ");
+    scanf("%d", &item.stock_quantity);
+}
+
+void check_output() {
+    printf("Status: ");
+    if (profit_percent > 0) {
+        printf("Profit");
+    } else if (profit_percent < 0) {
+        printf("Loss");
+    } else {
+        printf("Break Even");
+    }
+}
+
+void printf_type() {
+    printf("INVENTORY PROFIT REPORT\n");
+    printf("ID: %d\n", item.item_id);
+    printf("Cost: %.2f\n", item.cost_price);
+    printf("Sell: %.2f\n", item.sell_price);
+    printf("Quantity: %d\n", item.stock_quantity);
+    printf("Total Profit: %.2f\n", total_profit);
+    printf("Profit Percentage: %.2f%%\n", profit_percent);
 }
