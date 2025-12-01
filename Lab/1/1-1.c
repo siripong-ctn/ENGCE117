cc3c7f07742c4c9cb3896e65bdd3c45ef07fb946
siripong-ctn <siripong.2320@gmail.com>
2025-12-01 17:46:34 +0700

    Lab 1-1

---
+++ Lab/1/1-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int sum, i;
+
+    printf("Enter integer for loop: ");
+    scanf("%d", &sum);
+
+    for (i = 0; i < sum; i++) {
+        printf("[%d] Hello World\n", i);
+    }
+
+    return 0;
+}
