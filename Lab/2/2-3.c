dd11e1387e5616e9cfb4d5ec2a4e5beca14754ae
siripong-ctn <siripong.2320@gmail.com>
2025-12-10 16:20:05 +0700

    Lab 2-3

---
+++ Lab/2/2-3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+void explode(char str1[], char splitter, char str2[][10], int *count);
+
+int main() {
+    char out[20][10];
+    int num = 0;
+
+    explode("I/Love/You", '/', out, &num);
+
+    for(int i = 0; i < num; i++) {
+        printf("%d: %s\n", i + 1, out[i]);
+    }
+
+    return 0;
+}
+
+void explode(char str1[], char splitter, char str2[][10], int *count) {
+    int i = 0, j = 0, k = 0;
+
+    while (str1[i] != '\0') {
+        if (str1[i] == splitter) {
+            str2[j][k] = '\0';
+            j++;
+            k = 0;
+        } else {
+            str2[j][k] = str1[i];
+            k++;
+        }
+        i++;
+    }
+
+    str2[j][k] = '\0';
+    j++;
+
+    *count = j;
+}
