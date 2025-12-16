d41a02939d0f16e4cc21f1f5562a5894a6a39d78
siripong-ctn <siripong.2320@gmail.com>
2025-12-16 09:29:25 +0700

    Update 7-1

---
+++ 7/7-1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void handling();
+FILE *fptr;
+int year = 2025;
+char FILENAME[100];
+
+int main() {
+    handling();
+
+    if (fptr == NULL) {
+        printf("ERROR: Could not open file %s for writing.\n", FILENAME);
+        return 1;
+    }
+
+    printf("---FILE WRITING PROCESS---\n");
+    printf("Writing data to %s...\n", FILENAME);
+
+    fprintf(fptr, "Hello, C File Handling is easy.\nYear: %d", year);
+
+    printf("File writing complete.\n");
+    fclose(fptr);
+    printf("\n---FILE OPERATION REPORT---\n");
+    printf("Action: File created and data written successfully.\n");
+    printf("Filename: %s\n", FILENAME);
+
+    return 0;
+}
+
+void handling() {
+    printf("Enter file name: ");
+    scanf("%s", FILENAME);
+    fptr = fopen(FILENAME, "w");
+}
