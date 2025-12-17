5c576f1e438d717fbbe69122246ac308565c5bf9
siripong-ctn <siripong.2320@gmail.com>
2025-12-17 22:19:25 +0700

    Lab 3-1

---
+++ Lab/3/3-1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+struct student 
+{
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+};
+
+void upgrade(struct student *child);
+
+int main() {
+    struct student aboy;
+    struct student agirl;
+    aboy.sex = 'M';
+    aboy.gpa = 3.00;
+
+    agirl.sex = 'F';
+    agirl.gpa = 3.00;
+    upgrade(&aboy);
+    upgrade(&agirl);
+    printf("Aboy has gpa: %.2f\n", aboy.gpa);
+    printf("Agirl has gpa: %.2f", agirl.gpa);
+
+    return 0;
+}
+
+void upgrade(struct student *child) {
+    switch ((*child).sex) {
+    case 'M':
+        (*child).gpa *= 1.1;
+        break;
+    case 'F':
+        (*child).gpa *= 1.2;
+        break;
+    default:
+        break;
+    }
+}
