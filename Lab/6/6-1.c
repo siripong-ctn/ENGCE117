89a31896d743b651704d98b2419bdcdc3bdd2e65
siripong-ctn <siripong.2320@gmail.com>
2026-01-16 20:24:27 +0700

    Lab 6-1

--- Lab/6/6-1.c
+++ Lab/6/6-1.c
@@ -12,76 +12,39 @@ struct studentNode {
 };
 
 void ShowAll(struct studentNode *walk);
-struct studentNode *AddNode(struct studentNode **start, char *name, int age, char sex, float gpa);
 void InsNode(struct studentNode *walk, char *name, int age, char sex, float gpa, struct studentNode **begin);
 void GoBack(struct studentNode **walk);
 void DelNode(struct studentNode **walk, struct studentNode **begin);
+struct studentNode *AddNode(struct studentNode **start, char *name, int age, char sex, float gpa);
 
 int main() {
     struct studentNode *start = NULL, *now = NULL;
 
-    printf("*Add*\n");
-    now = AddNode(&start, "one", 6, 'M', 3.11);
-    ShowAll(start);
+    now = AddNode(&start, "one", 6, 'M', 3.11);ShowAll(start);
 
-    printf("*Add*\n");
-    now = AddNode(&start, "two", 8, 'F', 3.22);
-    ShowAll(start);
+    now = AddNode(&start, "two", 8, 'F', 3.22);ShowAll(start);
 
-    printf("*Insert*\n");
-    InsNode(now, "three", 10, 'M', 3.33, &start);
-    ShowAll(start);
+    InsNode(now, "three", 10, 'M', 3.33, &start);ShowAll(start);
 
-    printf("*Insert*\n");
-    InsNode(now, "four", 12, 'F', 3.44, &start);
-    ShowAll(start);
+    InsNode(now, "four", 12, 'F', 3.44, &start);ShowAll(start);
 
-    printf("*GoBack*\n");
     GoBack(&now);
 
-    printf("*Delete*\n");
-    DelNode(&now, &start);
-    ShowAll(start);
+    DelNode(&now, &start);ShowAll(start);
 
-    printf("*Delete*\n");
-    DelNode(&now, &start);
-    ShowAll(start);
+    DelNode(&now, &start);ShowAll(start);
 
-    printf("*Delete*\n");
-    DelNode(&now, &start);
-    ShowAll(start);
+    DelNode(&now, &start);ShowAll(start);
 
     return 0;
 }
 
 void ShowAll(struct studentNode *walk) {
     while (walk != NULL) {
-        printf("[%s] ", walk->name);
+        printf("%s ", walk->name);
         walk = walk->next;
     }
     printf("\n");
-}
-
-struct studentNode *AddNode(struct studentNode **start, char *name, int age, char sex, float gpa) {
-    struct studentNode *newNode = malloc(sizeof(struct studentNode));
-
-    strcpy(newNode->name, name);
-    newNode->age = age;
-    newNode->sex = sex;
-    newNode->gpa = gpa;
-    newNode->next = NULL;
-    newNode->back = NULL;
-
-    if (*start == NULL) {
-        *start = newNode;
-    } else {
-        struct studentNode *walk = *start;
-        while (walk->next != NULL)
-            walk = walk->next;
-        walk->next = newNode;
-        newNode->back = walk;
-    }
-    return newNode;
 }
 
 void InsNode(struct studentNode *walk, char *name, int age, char sex, float gpa, struct studentNode **begin) {
@@ -91,7 +54,6 @@ void InsNode(struct studentNode *walk, char *name, int age, char sex, float gpa,
     newNode->age = age;
     newNode->sex = sex;
     newNode->gpa = gpa;
-
     newNode->next = walk;
     newNode->back = walk->back;
 
@@ -114,7 +76,7 @@ void DelNode(struct studentNode **walk, struct studentNode **begin) {
     if (*walk == NULL) {
         return;
     }
-        
+
     struct studentNode *temp;
 
     if ((*walk)->back != NULL) {
@@ -132,4 +94,26 @@ void DelNode(struct studentNode **walk, struct studentNode **begin) {
 
     free(*walk);
     *walk = temp;
+}
+
+struct studentNode *AddNode(struct studentNode **start, char *name, int age, char sex, float gpa) {
+    struct studentNode *newNode = malloc(sizeof(struct studentNode));
+
+    strcpy(newNode->name, name);
+    newNode->age = age;
+    newNode->sex = sex;
+    newNode->gpa = gpa;
+    newNode->next = NULL;
+    newNode->back = NULL;
+
+    if (*start == NULL) {
+        *start = newNode;
+    } else {
+        struct studentNode *walk = *start;
+        while (walk->next != NULL)
+            walk = walk->next;
+        walk->next = newNode;
+        newNode->back = walk;
+    }
+    return newNode;
 }
